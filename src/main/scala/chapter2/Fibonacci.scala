5679912ca4f26237651aa2b30c2f36392fc760c0
macgrewal <7713555+macgrewal@users.noreply.github.com>
2023-07-22 17:11:43 +0100

    Minor refactor on Fibonacci code and tests

--- src/main/scala/chapter2/Fibonacci.scala
+++ src/main/scala/chapter2/Fibonacci.scala
@@ -5,10 +5,13 @@ import scala.annotation.tailrec
 trait Fibonacci {
   def atIndex(index: BigInt): BigInt = {
 
+    // Needs to be Pascal cased for pattern matching
+    val Zero = BigInt(0)
+
     // Pattern matching on BigInt: http://dhruba.name/2013/10/22/scala-cookbook-pattern-matching-on-bigint/
     @tailrec
     def loop(position: BigInt, current: BigInt, next: BigInt): BigInt = position match {
-      case p: BigInt if p == 0 => current
+      case Zero => current
       case _ => loop(position - 1, next, current + next)
     }
 
--- src/test/scala/chapter2/exercise-2.1.scala
+++ src/test/scala/chapter2/exercise-2.1.scala
@@ -8,17 +8,15 @@ class FibonacciSpec extends UnitSpec {
 
     // sequence taken from https://en.wikipedia.org/wiki/Fibonacci_number#List_of_Fibonacci_numbers
     val knownFibSequence = List(0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765)
-    for (i <- 0 until 19) {
-      assertResult(knownFibSequence(i)) {
-        Fibonacci.atIndex(i)
-      }
+    knownFibSequence.zipWithIndex.foreach {
+      case (result, index) => assertResult(result)(Fibonacci.atIndex(index))
     }
   }
 
   "The 100th number in the Fibonacci sequence" should "be 218922995834555169026" in {
 
-      assertResult(BigInt("218922995834555169026")) {
-        Fibonacci.atIndex(99)
+    assertResult(BigInt("218922995834555169026")) {
+      Fibonacci.atIndex(99)
     }
   }
 }
